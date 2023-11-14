164103e7be7bf55a1b807683949365670e0bf770
Fernanda <fernanda.zimmer@betternow.com.br>
2023-11-14 16:58:43 -0300

    Pastas atualizadas

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,9 @@ require 'site_prism'
 require 'site_prism/all_there'
 require 'rspec'
 require 'rspec/expectations'
+require 'roo'
+require 'fileutils'
+require 'open-uri'
 require_relative 'page.initialize.rb'
 include RSpec::Matchers
 
@@ -12,6 +15,6 @@ World(Page)
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
-    config.app_host = 'http://localhost:3434'
+    config.app_host = 'http://localhost:3434/cars-app/'
     config.default_max_wait_time = 5
 end
